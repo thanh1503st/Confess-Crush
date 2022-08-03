bc1cbf9e56fece1a138072a5f4a9c7db02d86aba
thanh1503st <110564428+thanh1503st@users.noreply.github.com>
2022-08-04 02:30:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'cho anh biết trong mess nhé <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay , chơi game nha bé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/profile.php?id=100012438708482' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
